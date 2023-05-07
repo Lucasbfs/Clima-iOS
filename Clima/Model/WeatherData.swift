f92a0a6476386b74706053f8dbf297b2714063ef
Lucas <lucasbfronciani@gmail.com>
2023-05-07 12:43:40 -0500

    first commit

---
+++ Clima/Model/WeatherData.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Lucas Fronciani on 11/17/22.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ Clima/Model/WeatherModel.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherModel.swift
+//  Clima
+//
+//  Created by Lucas Fronciani on 11/23/22.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
